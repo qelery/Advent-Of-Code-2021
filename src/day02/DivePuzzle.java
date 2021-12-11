e8a5cc0f44f83f020840e1463707d6194fe0869f
Quincy Elery <quincyelery@gmail.com>
2021-12-11 17:26:09 -0600

    [Day02] Part 2 solved - parsing movement and aim instructions

--- src/day02/DivePuzzle.java
+++ src/day02/DivePuzzle.java
@@ -11,8 +11,10 @@ public class DivePuzzle extends DailyPuzzle {
 
     public static void main(String[] args) {
         List<String> lines = readFile("day02.txt");
-        Point point = parseInstructions1(lines);
-        System.out.println("Part 1 - Final depth time horizontal position: " + point.x * point.y);
+        Point point1 = parseInstructions1(lines);
+        System.out.println("Part 1 - Final depth time horizontal position: " + point1.x * point1.y);
+        Point point2 = parseInstructions2(lines);
+        System.out.println("Part 2 - Final depth time horizontal position: " + point2.x * point2.y);
     }
 
     /**
@@ -30,6 +32,31 @@ public class DivePuzzle extends DailyPuzzle {
                 case "up" -> y -= distance;
                 case "down" -> y += distance;
                 case "forward" -> x += distance;
+                default -> throw new IllegalStateException("Invalid direction: " + direction);
+            }
+        }
+        return new Point(x, y);
+    }
+
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
+    public static Point parseInstructions2(List<String> instructions) {
+        int x = 0;
+        int y = 0;
+        int aim = 0;
+        for (String instr : instructions) {
+            String[] parts = instr.split(" ");
+            String direction = parts[0];
+            int amount = Integer.parseInt(parts[1]);
+            switch (direction) {
+                case "up" -> aim -= amount;
+                case "down" -> aim += amount;
+                case "forward" -> {
+                    x += amount;
+                    y += aim * amount;
+                }
                 default -> throw new IllegalStateException("Invalid direction: " + direction);
             }
         }
