f5bc7ed5af72d431f777e08806d321fa846679f3
Quincy Elery <quincyelery@gmail.com>
2021-12-11 16:25:18 -0600

    [Day01] solve part 2 - count increasing sliding windows

--- src/day01/SonarSweepPuzzle.java
+++ src/day01/SonarSweepPuzzle.java
@@ -3,7 +3,6 @@ package day01;
 import util.Conversions;
 import util.DailyPuzzle;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class SonarSweepPuzzle extends DailyPuzzle {
@@ -12,6 +11,7 @@ public class SonarSweepPuzzle extends DailyPuzzle {
         List<String> lines = readFile("day01.data");
         int[] numbers = Conversions.convertToIntArray(lines);
         System.out.println("Number of increasing: " + countIncreasing(numbers));
+        System.out.println("Number of increasing sliding window: " + countIncreasingSlidingWindows(numbers));
     }
 
     /**
@@ -30,5 +30,22 @@ public class SonarSweepPuzzle extends DailyPuzzle {
             last = current;
         }
         return count;
+    }
+
+    /**
+     *
+     */
+    public static int countIncreasingSlidingWindows(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            return 0;
+        }
+
+        int[] runningTotals = new int[numbers.length-2];
+        for (int i = 0; i < runningTotals.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                runningTotals[i] += numbers[i + j];
+            }
+        }
+        return countIncreasing(runningTotals);
     }
 }
